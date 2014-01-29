463da81af597019eb167831b27cfd6158470ae77
Hector Leiva <hector@hectorleiva.com>
2014-01-28 23:44:47 -0500

    adding bootstrap, adding images, adding jQuery functionality to actually calculate the costs per 10 minutes X amount of days since the loan was introduced. Added jQuery UI, datepicker.

--- custom.js
+++ custom.js
@@ -10,12 +10,41 @@
 
  */
 jQuery(document).ready(function($) {
-    var quantity, errmsg, submit, final_quantity, compounds, original_loan_date;
+  var body, quantity, errmsg, submit, amount, loan_question_title, loan_questionaire, final_quantity, compounds, original_loan_date, todays_date, day_diff, n, money, kaiji_reaction, credits;
 
-    quantity = $('#quantity');
-    errmsg = $('#errmsg');
-    submit = $('#submit');
-    original_loan_date = $('#loan_date');
+  body = $('body');
+  quantity = $('#quantity');
+  errmsg = $('#errmsg');
+  submit = $('#submit');
+  loan_question_title = $('.loan_question_title');
+  original_loan_date = $('#original_loan_date');
+  money = $('.money');
+  due = $('#due');
+  amount_due = $('#amount_due');
+  loan_questionaire = $('#loan_questionaire');
+  kaiji_reaction = $('#kaiji_reaction .okay');
+  kaiji_output = $('#kaiji_reaction .fuuu');
+  credits = $('#credits');
+
+  /* Courtesy dubroe https://github.com/dubroe/bootstrap-money-field*/
+  $.fn.money_field = function(options) {
+    var defaults = {
+      width: null
+    };
+    var options = $.extend(defaults, options);
+    
+    return this.each(function() {
+      obj = $(this);
+      if(options.width)
+        obj.css('width', options.width + 'px');
+      obj.wrap("<div class='input-prepend'>");
+      obj.before("<span class='add-on'>$</span>");
+    });
+  };
+  money.money_field();
+
+  //  Make the loan respond to datepicker
+  original_loan_date.datepicker();
 
   //called when key is pressed in textbox
   quantity.keypress(function (e) {
@@ -23,12 +52,65 @@ jQuery(document).ready(function($) {
      if (e.which != 8 && e.which !== 0 && (e.which < 48 || e.which > 57)) {
         //display error message
         errmsg.html("Digits Only").show().fadeOut("slow");
-               return false;
+          return false;
     }
    });
+
   submit.on('click', function() {
-    original_loan_date = original_loan_date.val();
-    compounds = 10; // Should be compand every 10 minutes from original loan date.
-    final_quantity = (quantity.val() * Math.pow((1+'.'+(Math.random()*11)), compounds));
+
+    if (original_loan_date.val().length === 0) {
+      original_loan_date.addClass('alert-danger');
+      return;
+    } else if (original_loan_date.hasClass('alert-danger')) {
+      original_loan_date.removeClass('alert-danger');
+    }
+
+    if (quantity.val().length === 0) {
+      quantity.addClass('alert-danger');
+      return;
+    } else if (quantity.hasClass('alert-danger')) {
+      quantity.removeClass('alert-danger');
+    }
+
+    //  Get todays date
+    n = new Date();
+    todays_date = (n.getMonth()+1) + '/';
+    todays_date += n.getDate() + '/';
+    todays_date += n.getFullYear();
+
+    function parseDate(str) {
+      var mdy = str.split('/');
+      return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    }
+
+    function daydiff(first, second) {
+      return (second-first)/(1000*60*60*24);
+    }
+
+    day_diff = daydiff(parseDate(original_loan_date.val()), parseDate(todays_date));
+    day_diff = ((day_diff*24*60)/6);
+
+    compounds = (10 * day_diff); // Should be compand every 10 minutes from original loan date.
+    console.log('quantity is ' + quantity.val());
+    final_quantity = (quantity.val() * compounds);
+    if ( !$.isNumeric(final_quantity) ) {
+      console.log('this is not a number, what is this? ' + final_quantity);
+    } else {
+      final_quantity = final_quantity.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+    }
+    loan_questionaire.slideToggle( "slow", function() {
+      amount_due
+        .text('$'+final_quantity);
+      due
+        .addClass('black_outline')
+        .slideToggle("slow");
+      kaiji_reaction.hide();
+      body.addClass('red');
+      credits
+        .addClass('white')
+        .addClass('black_outline');
+      kaiji_output.show();
+    });
   });
+
 });
